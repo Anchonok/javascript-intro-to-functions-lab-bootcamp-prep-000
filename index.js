d157e2662db5a840a51b62b3a4a29144ef80f61c
Anna Dubov <a.chistyakova@gmail.com>
2019-03-27 15:20:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if ('hello')
+  if ( string === 'hello')
   return ("I can't hear you!");
-  else if('HELLO')
+  else if( string === 'HELLO')
   return ("YES INDEED!");
   else ('I love you, Grandma.')
   return ("I love you, too.")
